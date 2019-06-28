78b45722f045bc9ab05dcf0939f88552b2124118
leonardjin <450451870qq@gmail.com>
2019-06-28 18:51:08 +0800

    更新nameserver

--- example/src/main/java/demo/MultiThreadTest.java
+++ example/src/main/java/demo/MultiThreadTest.java
@@ -12,8 +12,13 @@ import java.util.concurrent.*;
 public class MultiThreadTest {
     public static void main(String[] args) throws InterruptedException, MQClientException {
         Producer producer = new Producer();
-        // 设置 Name Server地址，在控制台上获取， 必须设置
-        producer.setNameServerAddress("xxx");
+        // 设置 Name Server地址。必须设置 不同地域不同网络不同 
+        // 地域对应的缩写 bj:北京 sh：上海 gz:广州 in:孟买 ca:北美 cd:成都 cq: 重庆
+        //  hk:香港 kr:韩国 ru:俄罗斯 sg:新加坡 shjr:上海金融 szjr:深圳金融 th:曼谷 use: 弗吉尼亚 usw： 美西 
+        // 私有网络地址：http://cmq-nameserver-vpc-{region}.api.tencentyun.com 支持腾讯云私有网络的云服务器内网访问
+        // 公网地址：    http://cmq-nameserver-{region}.tencentcloudapi.com
+        producer.setNameServerAddress("http://cmq-nameserver-xxx.tencentcloudapi.com");
+
         // 设置SecretId，在控制台上获取，必须设置
         producer.setSecretId("xxx");
         // 设置SecretKey，在控制台上获取，必须设置
